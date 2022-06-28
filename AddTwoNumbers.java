2ea7a9fe3eacf7c5934d06572576f38e082df16c
SebastianPetrea <80840012+SebastianPetrea@users.noreply.github.com>
2022-06-28 13:06:47 +0200

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		// creates an object of Scanner
+	    Scanner s = new Scanner(System.in);
+
+	 // takes input from the keyboard
+	    int a = s.nextInt(), b = s.nextInt();
+	    
+	 // prints the result
+	    System.out.println(a + b);
+
+	    // closes the scanner
+	    s.close();
+		
+	}
+
+}
