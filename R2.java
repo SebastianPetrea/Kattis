9ad88a641157160e0a2c3797ec359ede371e4ea2
SebastianPetrea <80840012+SebastianPetrea@users.noreply.github.com>
2022-08-25 11:46:38 +0200

    Add files via upload

---
+++ R2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class R2
+{
+  public static void main(String[] args) {
+	  Scanner scan= new Scanner (System.in) ;
+	  int a = scan.nextInt(), b = scan.nextInt();
+      System.out.print(b * 2 - a);
+      }
+   }
+  
+
